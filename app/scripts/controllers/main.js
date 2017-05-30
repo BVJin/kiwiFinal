ab5850b590d9cdc7ce813acbd46f2ee7cb07adf0
Sanchuan Jin <jsanchuan@gmail.com>
2017-05-30 12:43:39 -0500

    Initial the app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yiliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yiliApp
+ */
+angular.module('yiliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
